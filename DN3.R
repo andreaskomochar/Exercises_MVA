84cdb8bd77126b78a1bf9f366193c5a2ce2dec4a
wishmaster75 <andreaskomochar@gmail.com>
2014-12-09 20:14:03 +0100

    Update DN3.R

--- DN3.R
+++ DN3.R
@@ -11,33 +11,6 @@ hc
 plot(hc, hang = -1)
 
 
-#######################################################
-podatki <- read.table("jaccard.txt")
-
-J_razdalja <- function(x,y) {
-  
-  M11 <- 0
-  M01 <- 0
-  M10 <- 0
-  
-  for (j in 1:10)  {
-      if (podatki[x,j] == 1 & podatki[y,j] == 1) {
-        M11 <- M11+1     
-    }         
-      else if (podatki[x,j] == 1 & podatki[y,j] == 0) {
-        M10 <- M10+1}
-    
-      else if (podatki[x,j] == 0 & podatki[y,j] == 1) {
-        M01 <- M01+1}    
-    
-  }
-  
-
-J <- (M01+M10)/(M01+M10+M11)
-
-return(J)
-
-}
 
 
 
